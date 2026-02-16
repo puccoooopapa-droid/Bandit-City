8b95c1afc5da03e8b7bb0ad57e9d36a6c80e5c23
puccoooopapa-droid <puccoooopapa@gmail.com>
2026-02-16 10:29:33 +0200

    Implement startup event to start bot
    Add startup event to launch bot in background task.

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 from webapp.api import app
+import asyncio
+import main
+
+@app.on_event("startup")
+async def startup_event():
+    # Запускаем бота в фоновой задаче при старте сервера
+    asyncio.create_task(main.start_bot())
